ea5e7335a80a7bf41a2565f094784b122d8a0d6a
noeliaariza <noelia.ariza@gmail.com>
2024-05-22 17:55:37 +0200

    home styles added

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} />
+            <img src={assets.twitter_icon} />
+            <img src={assets.linkedin_icon} />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+34-687-698-877</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
