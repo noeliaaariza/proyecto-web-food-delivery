56590c33351c47d7ab18b550df0792f9c1a8c2c8
noeliaariza <noelia.ariza@gmail.com>
2024-05-23 17:29:55 +0200

    remove from cart functionality

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
   const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
   return (
-    <div>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -20,13 +20,23 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div key={index} className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{item.price * cartItems[item._id]} €</p>
-                <p>x</p>
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price} €</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]} €</p>
+                  <p
+                    onClick={() => {
+                      removeFromCart(item._id);
+                    }}
+                    className="cross"
+                  >
+                    x
+                  </p>
+                </div>
+                <hr />
               </div>
             );
           }
